6ac8c558a1b9759461454c8370a10aeb522e108a
Spencer Freebairn <spencerfreebairn@Spencers-Mac-mini.local>
2019-04-30 14:54:05 -0700

    changed dark theme color value

--- swap-theme.js
+++ swap-theme.js
@@ -1,7 +1,7 @@
 document.getElementById('swap-theme-button').onclick = function() {
    var element = document.getElementById('test');
 
-   var darkTheme = "rgb(32, 32, 32)";
+   var darkTheme = "rgb(10, 10, 32)";
    var lightTheme = "rgb(255, 255, 255)";
    var currentTheme = window.getComputedStyle(document.getElementById('test')).backgroundColor;
 
